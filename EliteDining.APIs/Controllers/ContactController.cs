a960747a9c6bce9f2e697b5f095832c2de00c566
MOHAMED-MOSTAFA-AMEEN <nooramin1992uu@gmail.com>
2023-11-18 00:06:04 +0200

    Contact put update

--- EliteDining.APIs/Controllers/ContactController.cs
+++ EliteDining.APIs/Controllers/ContactController.cs
@@ -31,13 +31,13 @@ namespace EliteDining.APIs.Controllers
         {
             try
             {
-                var Contacts = await _ContactService.GetAllAsync();
-                if (Contacts != null)
+                var contacts = await _ContactService.GetAllAsync();
+                if (contacts != null)
                 {
                     return TypedResults.Ok(new ResponseDataModel<IEnumerable<ContactViewModel>>
                     {
                         Success = true,
-                        Data = _mapper.Map<IEnumerable<Contact>, IEnumerable<ContactViewModel>>(Contacts)
+                        Data = _mapper.Map<IEnumerable<Contact>, IEnumerable<ContactViewModel>>(contacts)
 
                     });
                 }
@@ -62,13 +62,13 @@ namespace EliteDining.APIs.Controllers
         {
             try
             {
-                var Contact = await _ContactService.GetByIdAsync(id);
-                if (Contact != null)
+                var contact = await _ContactService.GetByIdAsync(id);
+                if (contact != null)
                 {
                     return TypedResults.Ok(new ResponseDataModel<ContactViewModel>
                     {
                         Success = true,
-                        Data = _mapper.Map<Contact, ContactViewModel>(Contact)
+                        Data = _mapper.Map<Contact, ContactViewModel>(contact)
 
                     });
                 }
@@ -91,10 +91,10 @@ namespace EliteDining.APIs.Controllers
         [HttpPost]
         public async Task<IResult> Create(ContactViewModel ContactViewModel)
         {
-            var Contact = _mapper.Map<ContactViewModel, Contact>(ContactViewModel);
+            var contact = _mapper.Map<ContactViewModel, Contact>(ContactViewModel);
             try
             {
-                var isSaved = await _ContactService.AddAsync(Contact);
+                var isSaved = await _ContactService.AddAsync(contact);
                 if (isSaved == 1)
                 {
                     return TypedResults.Ok(new ResponseModel
@@ -122,7 +122,7 @@ namespace EliteDining.APIs.Controllers
         [HttpPut("{id}")]
         public async Task<IResult> Update(int id, ContactViewModel ContactViewModel)
         {
-            var Contact = _mapper.Map<ContactViewModel, Contact>(ContactViewModel);
+            var contact = _mapper.Map<ContactViewModel, Contact>(ContactViewModel);
 
 
             
@@ -131,7 +131,8 @@ namespace EliteDining.APIs.Controllers
                 var existingContact = await _ContactService.GetByIdAsync(id);
                 if (existingContact != null)
                 {
-                    await _ContactService.UpdateAsync(Contact);
+                contact.Id = id;
+                    await _ContactService.UpdateAsync(contact);
                     return TypedResults.Ok(new ResponseModel
                     {
                         Success = true,
@@ -156,8 +157,8 @@ namespace EliteDining.APIs.Controllers
         {
             try
             {
-                var Contact = await _ContactService.GetByIdAsync(id);
-                if (Contact != null)
+                var contact = await _ContactService.GetByIdAsync(id);
+                if (contact != null)
                 {
                     await _ContactService.DeleteAsync(id);
                     return TypedResults.Ok(new ResponseDataModel<ContactViewModel>
